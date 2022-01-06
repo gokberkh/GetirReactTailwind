23938592770778e272e707f2935895523aa2202c
Gökberk Hıdıroğlu <gokberkhdrgl@gmail.com>
2022-01-06 16:37:26 +0300

    tailwindcss installation

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "brand-color": "#4c3398",
+        "primary-brand-color": "#5d3ebc",
+        "secondary-brand-color": "#7849f7",
+        "brand-yellow": "#ffd300",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
