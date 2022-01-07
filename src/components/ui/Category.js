6c601c1f71a338894e36fe05f6803e1690537a18
Gökberk Hıdıroğlu <gokberkhdrgl@gmail.com>
2022-01-07 15:18:16 +0300

    category,title component created.

---
+++ src/components/ui/Category.js
@@ -0,0 +1,17 @@
+export default function Category({ category: { id, title, image } }) {
+  return (
+    <a
+      href="#/"
+      className="flex flex-col items-center group text-center p-4 gap-y-2 transition hover:bg-purple-50"
+    >
+      <img
+        className="w-12 h-12 rounded border-gray-200"
+        src={image}
+        alt={title}
+      />
+      <span className="text-sm font-semibold text-gray-700 whitespace-nowrap group-hover:text-brand-color tracking-tight ">
+        {title}
+      </span>
+    </a>
+  );
+}
