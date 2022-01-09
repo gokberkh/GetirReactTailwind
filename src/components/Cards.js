0e4b5891b949c945c02aba2e837605ecbd7c1753
Gökberk Hıdıroğlu <gokberkhdrgl@gmail.com>
2022-01-09 17:43:57 +0300

    cards fixed and fakeapi created.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,3 +1,29 @@
+import { useState, useEffect } from "react";
+import cardsData from "fakeApi/cards.json";
+
 export default function Cards() {
-  return <div>Cards</div>;
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    setCards(cardsData);
+  }, []);
+
+  return (
+    <div className="grid grid-cols-3 gap-x-4 w-3/4 container mx-auto ">
+      {cards.length &&
+        cards.map((card) => (
+          <div className="bg-white p-14 rounded-lg shadow-md flex flex-col items-center text-center ">
+            <img
+              alt=""
+              className="h-[150px] w-[150px] mb-6 "
+              src={card.image}
+            />
+            <h6 className="font-semibold text-lg text-primary-brand-color">
+              {card.title}
+            </h6>
+            <p className="mt-2 text-gray-700">{card.description}</p>
+          </div>
+        ))}
+    </div>
+  );
 }
