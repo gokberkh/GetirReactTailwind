4747e63c891d1f95b8b60e4e1612bd190143ce62
Gökberk Hıdıroğlu <gokberkhdrgl@gmail.com>
2022-01-11 17:14:33 +0300

    react collapse added.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# getirClone
+# Getir React Tailwind
+
+[![image](https://i.hizliresim.com/r9b765d.png)](https://hizliresim.com/r9b765d)
+
+I coded the `getir.com` homepage using react and tailwind.
+
+## Used
+
+`React` - `Tailwind`
+
+## Demo
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       spacing: {
         0.1: "0.063rem",
